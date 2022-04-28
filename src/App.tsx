d2ecfd232bdde78df8c71bee6cc85cf2ba32cad3
Gm <zakuzzdj@gmail.com>
2022-04-28 16:14:37 +0000

    ref: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Route from "./routes";
 
 function App() {
   return (
-    <Route />
+    <Router>
+      <Route />
+    </Router>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -12,14 +12,16 @@ import reportWebVitals from "./reportWebVitals";
 // } else {
 // }
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = createRoot(document.getElementById("root")!)
+
+container.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
