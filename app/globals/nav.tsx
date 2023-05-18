d690759ef7a7bbfa43c32c63572ff8afb6b10861
Gautam <zakuzzdj@gmail.com>
2023-05-18 07:45:00 +0000

    init ✨

---
+++ app/globals/nav.tsx
@@ -0,0 +1,14 @@
+export function Nav() {
+  return (
+    <>
+          <div>
+              <ul>
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/"></a></li>
+                  <li><a href="/">Home</a></li>
+              </ul>
+      </div>
+    </>
+  );
+}
